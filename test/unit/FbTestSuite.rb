dec3b57fc4d0e9ad8a9e00ff9b9e5ae098e1edd0
brent <brent@e6efdd46-4312-0410-9032-dd8a18d5a4db>
2006-06-01 19:10:58 +0000

    Move transaction tests to their own file.
    git-svn-id: https://svn.ttiltd.com/svn/dev.rb/trunk/libraries/fb@124 e6efdd46-4312-0410-9032-dd8a18d5a4db

--- test/unit/FbTestSuite.rb
+++ test/unit/FbTestSuite.rb
@@ -1,5 +1,5 @@
 $: << File.dirname(__FILE__)
 require 'test/unit'
-%w{ Database Connection Cursor DataTypes }.each do |f|
+%w{ Database Connection Cursor DataTypes Transaction }.each do |f|
   require "#{f}TestCases"
 end
