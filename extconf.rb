4dad6f31e383ff3b517be01f5044ff611f261706
brent <brent@e6efdd46-4312-0410-9032-dd8a18d5a4db>
2006-06-04 22:38:01 +0000

    Remove Japanese comments. I have no way to ascertain whether they are current or relevant.
    git-svn-id: https://svn.ttiltd.com/svn/dev.rb/trunk/libraries/fb@143 e6efdd46-4312-0410-9032-dd8a18d5a4db

--- extconf.rb
+++ extconf.rb
@@ -5,13 +5,6 @@ require 'mkmf'
 
 libs = %w/ gdslib gds /  # InterBase library
 
-# 注意: InterBase を導入せずに Firebird を導入したときでも
-# libgds.so が存在するかもしれません。
-# 例えば rpm -U FirebirdSS-1.5.1.4481-0.i686.nptl.rpm すると
-#   /usr/lib/libgds.so -> /opt/firebird/lib/libfbclient.so
-#   /usr/lib/libgds.so.0 -> /opt/firebird/lib/libfbclient.so
-# が作られます。
-
 fbclientlib =  # Firebird library
   case RUBY_PLATFORM
   when /bccwin32/
@@ -28,8 +21,6 @@ test_func = "isc_attach_database"
 # for ruby-1.8.1 mkmf
 case RUBY_PLATFORM
 when /win/
-  # win32(VC++6) では header を指定しないと失敗します。
-  # また、ruby-1.8.1 mkmf は、have_library に headers を渡せません。
   libs.find {|lib| have_library(lib) } and
     have_func(test_func, ["ibase.h"])
 else
